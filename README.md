40a28377ec8139adec9571160fa6e54a5704a95e
tkvern <tkvern@qq.com>
2018-03-07 18:32:22 +0800

    add https mock

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Guider
+follow the step and run this projec
+
+Environmental requirements
+
+- nodejs v8.10.0
+
+## step1
+
+clone this project
+
+## step 2
+
+Installation dependent
+
+### server
+```
+$ cd server
+$ npm install // or yarn
+```
+
+### client
+```
+$ cd cline
+$ npm install // or yarn
+```
+
+## step 3
+
+read anther readme.md
--- server/api/ticker.controller.js
+++ server/api/ticker.controller.js
@@ -1,5 +1,36 @@
+var https = require('https');
+var querystring = require('querystring');
+
 exports.test = function (req, res) {
-    console.log(req.query);
-    console.log(res.url);
-    return res.status(200).json({ test: 'a' });
+    var page = req.query.page;
+    var size = req.query.size;
+    var data = {
+        page: page,
+        size: size
+    };
+
+    var content = querystring.stringify(data);
+
+    var options = {
+        hostname: 'qube.vip',
+        port: 443,
+        path: '/api/ticker',
+        method: 'GET'
+    }
+
+    var _data = "";
+    var _req = https.request(options, function (_res) {
+        _res.setEncoding('utf-8');
+        _res.on('data', function (chunk) {
+            _data += chunk;
+        });
+        _res.on('end', function () {
+            // console.log('响应结束********');
+            return res.status(200).json(JSON.parse(_data));
+        });
+    })
+    _req.on('error', function (err) {
+        console.error(err);
+    });
+    _req.end();
 }
