0d37f116a0d986e51d9ccc4e5fd1c9d440b79ae9
tkvern <tkvern@qq.com>
2018-03-16 19:40:10 +0800

    修改domain

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -99,7 +99,7 @@ export default {
   },
   methods: {
     async initData () {
-      let res = await axios.get('//superpass.ai//api/ticker', {
+      let res = await axios.get('//superpass.ai/api/ticker', {
         params: {
           page: 1,
           size: 100
